f768f2d91464607f1bb5693b90d09fde76718c65
Piotr Wawryka <piotrwpwsz@gmail.com>
2022-04-29 21:17:22 +0200

    example

---
+++ components/navbar.inc.php
@@ -0,0 +1,41 @@
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#"><img src="img/logo.png" alt="" /></a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+          class="collapse navbar-collapse justify-content-end"
+          id="navbarNav"
+        >
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link"  href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="services.php">Medykamenty</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="gallery.php">Raporty</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.php">About/Dokumentacja</a>
+            </li> 
+            <li class="nav-item"> 
+              <!-- <a class="nav-link" href="about.php" class="btn btn-outline-warning mb-3">Zaloguj</a> -->
+              <button type="button" class="btn btn-outline-warning mb-3">
+              Wyloguj
+            </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
