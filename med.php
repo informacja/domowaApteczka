8f9c92e3d37e9f07738c039ea9947c8c477471e5
Piotr Wawryka <piotrwpwsz@gmail.com>
2022-05-05 16:16:00 +0200

    fix die() and code leaking problem

--- med.php
+++ med.php
@@ -1,39 +1,45 @@
 <?php require('components/head.inc.php'); ?>
 <?php include('components/navbar.inc.php'); ?>
-
 <?php include('components/companies.inc.php'); ?>
-dodaj
-utylizuj
 
-<?
 
-require('config.php');
+<?php
 
-$conn =  mysqli_connect(
+require_once('config.php');  
+
+ $conn =  mysqli_connect(
     $hostname,
     $username,
     $password,
     $database
-);
+ );
 if (!$conn) {
+    echo "<h1>Error</h1>";
     die("Connection failed:" . mysqli_connect_error());
 }
-$user_email = mysqli_real_escape_string($conn, $user_email);
-$user_password = mysqli_real_escape_string($conn, $user_password);
+
+echo "dodaj
+utylizu";
+// $user_email = mysqli_real_escape_string($conn, $user_email);
+// $user_password = mysqli_real_escape_string($conn, $user_password);
 
 $sql_leki = "SELECT leki_specyfikacja_idleki, ilosc_kupiona, ilosc_pozostala, leki_w_apteczce.status FROM `leki_w_apteczce`";
 
 $sql_apteczki = "SELECT apteczki_name FROM apteczki";
-
+// $sql = "SELECT apteczki_name FROM `apteczki` WHERE apteczki_id='1'";
+      
 $res = mysqli_query($conn, $sql_leki);
+// var_dump($conn);
+// mysqli_close($conn) ;
+
 if ($res)
     echo "odebrano z bazy<br>";
 else
-    die("Błąd <br>" . mysqli_error($conn));
+    die("Błąd4 <br>" . mysqli_error($conn));
 
 if (mysqli_num_rows($res) > 0) {
     while ($record = mysqli_fetch_assoc($res)) {
-        // var_dump($record);
+        var_dump($record);
     }
 }
 
@@ -72,5 +78,5 @@ if (mysqli_num_rows($res) > 0) {
     </tbody>
 </table> -->
 
-<?php include('components/services.inc.php'); ?>
+<?php include('components/med.inc.php'); ?>
 <?php require('components/footer.inc.php'); ?>
